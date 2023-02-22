cbea4d3960cfca8656536b421ba2190395b8716d
Hreen-Fox <111928349+Hreen-Fox@users.noreply.github.com>
2023-02-22 19:21:00 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 main = () => {
-    console.log($($($('.row')[0]).find('.card')).outerHeight() - $($('.row')[0]).find('.card .bottom').outerHeight())
-    console.log($($($('.row')[0]).find('.card')).outerHeight())
-    console.log($($('.row')[0]).find('.card .bottom').outerHeight())
     $($('.row')[0]).find('.card .bottom').css('top', ($($($('.row')[0]).find('.card .top')).outerHeight() - $($('.row')[0]).find('.card .bottom').outerHeight()) +'px');
     $($('.row')[1]).find('.card .bottom').css('top', ($($($('.row')[1]).find('.card .top')).outerHeight() - $($('.row')[1]).find('.card .bottom').outerHeight())+'px');
     $('#firstBlock').css('marginTop', Number($('#firstBlock').css('margin-top').split('px')[0]) + $('header').height() + 'px')
